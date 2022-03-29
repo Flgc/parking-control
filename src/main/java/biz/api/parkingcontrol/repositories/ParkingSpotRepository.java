59d19d7872d57634ae8696be59d89e36c7d96c59
Fabio Luis <ml@fabiotecnico1.biz>
2022-03-29 20:32:23 -0300

    ParkingSpotDto created

--- src/main/java/biz/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/biz/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -2,8 +2,11 @@ package biz.api.parkingcontrol.repositories;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import biz.api.parkingcontrol.models.ParkingSpotModel;
 
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
 
 }
