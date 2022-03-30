e332f90d0561fc141066d592b8e1825ae767eaac
Fabio Luis <ml@fabiotecnico1.biz>
2022-03-30 20:44:49 -0300

    ParkingSpotService - Save method created

--- src/main/java/biz/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/biz/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,10 @@
 package biz.api.parkingcontrol.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+
+import biz.api.parkingcontrol.models.ParkingSpotModel;
 import biz.api.parkingcontrol.repositories.ParkingSpotRepository;
 
 @Service
@@ -11,5 +15,9 @@ public class ParkingSpotService {
 	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
 		this.parkingSpotRepository = parkingSpotRepository;
 	}
-	
+
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}	
 }
